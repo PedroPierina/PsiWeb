5f6ad660eb33fd6894eafb5098bf389fbf37f93a
PedroPierina <pierinapedro@gmail.com>
2020-05-11 16:05:19 -0300

    Login simples

--- src/main/java/com/pierina/psiweb/config/SecurityConfig.java
+++ src/main/java/com/pierina/psiweb/config/SecurityConfig.java
@@ -1,9 +1,14 @@
 package com.pierina.psiweb.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -11,6 +16,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-		http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+		http
+			.authorizeRequests()
+				.antMatchers("/", "/home").permitAll()
+				.antMatchers("/css/**", "/js/**", "/assets/**").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.permitAll();
+	}
+	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("admin")
+				.password("admin")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
 	}
 }
--- src/main/java/com/pierina/psiweb/controller/ViewController.java
+++ src/main/java/com/pierina/psiweb/controller/ViewController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
@@ -24,4 +24,10 @@ public class ViewController {
     public String loginPage(){
     	return "login";
 	}
+    
+//    @RequestMapping("/feed")
+//    public String feedPage(){
+//    	return "feed";
+//	}
+    
 }
